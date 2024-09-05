d50a7af12648f64b159d32a64fbd54c989acf626
Takeru Ohta <phjgt308@gmail.com>
2024-09-05 19:01:17 +0900

    Add Request trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,13 @@ mod io;
 mod types;
 
 pub use io::JsonlStream;
+use serde::{Deserialize, Serialize};
 pub use types::{
     ErrorCode, ErrorObject, JsonRpcVersion, RequestId, RequestObject, RequestParams, ResponseObject,
 };
+
+pub trait Request: Serialize + for<'a> Deserialize<'a> {
+    type Response: Serialize + for<'a> Deserialize<'a>;
+
+    fn is_notification(&self) -> bool;
+}
